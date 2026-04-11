a23623629dae61d95065963dce547cc6610f5df0
casidor <rvolosin0@gmail.com>
2026-04-11 15:26:52 +0300

    feat: add Entity base class with HP, Attack and TakeDamage

---
+++ Core/Models/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameCore.Models
+{
+    public abstract class Entity
+    {
+        public string Name { get; set; }
+        public int MaxHP { get; set; } = 0;
+        public int HP { get; set; } = 0;
+        public int Attack { get; set; } = 0;
+        public bool IsAlive => HP > 0;
+        public void TakeDamage(int damage)
+        {
+            HP = Math.Max(HP - damage, 0);
+        }
+    }
+}
