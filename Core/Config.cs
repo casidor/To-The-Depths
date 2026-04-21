ef7b86a037c9652d0749778ee36864ef7146f87a
casidor <rvolosin0@gmail.com>
2026-04-21 13:56:18 +0300

    chore: rebalance game config for better experience

--- Core/Config.cs
+++ Core/Config.cs
@@ -13,25 +13,25 @@ namespace GameCore
         public const int FieldWidth = 80;
         public const int FieldHeight = 25;
         //Player settings
-        public const int GoldAmount = 10;
         public const int PlayerMaxHP = 100;
         //Enemy settings
-        public const int EnemyDamage = 5;
+        public const int EnemyDamage = 15;
         public const int GoldStolen = 10;
-        public const int AggroRange = 5;
+        public const int AggroRange = 7;
         //Generation settings
         public const int MaxRooms = 10;
         public const int GenAttempts = 300;
         public const int MinRoomSize = 3;
-        public const int MaxRoomSize = 8;
-        public const int GoldChance = 10;
+        public const int MaxRoomSize = 10;
+        public const int GoldAmount = 5;
+        public const int GoldChance = 15;
         public const int KeysAmount = 3;
         public const int ExitAmount = 1;
-        public const int EnemiesAmount = 5;
-        public const int AltarsAmount = 3;
-        public const int MaxFloor = 1;
+        public const int EnemiesAmount = 7;
+        public const int AltarsAmount = 2;
+        public const int MaxFloor = 5;
         //Tiles settings
-        public const int AltarCharges = 3;
+        public const int AltarCharges = 2;
         public const int AltarHeal = 20;
         public const int HealCost = 20;
     }
