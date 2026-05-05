54c1705236e227d5d1fcc1b635073bf465cbcf22
casidor <rvolosin0@gmail.com>
2026-05-05 18:32:21 +0300

    feat: add app version display to menu

--- AvaloniaUI/AvaloniaUI/ViewModels/MenuViewModel.cs
+++ AvaloniaUI/AvaloniaUI/ViewModels/MenuViewModel.cs
@@ -1,10 +1,14 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
+using System.Reflection;
 
 namespace AvaloniaUI.ViewModels
 {
     public partial class MenuViewModel : ViewModelBase
     {
+        [ObservableProperty]
+        private string appVersion = Assembly.GetExecutingAssembly()?.GetName().Version != null ? $"v{Assembly.GetExecutingAssembly()?.GetName().Version.Major}.{Assembly.GetExecutingAssembly()?.GetName().Version.Minor}.{Assembly.GetExecutingAssembly()?.GetName().Version.Build}" : "v1.0.0";
         public event Action? StartGameRequested;
         public event Action? LoadGameRequested;
         public event Action? HelpRequested;
